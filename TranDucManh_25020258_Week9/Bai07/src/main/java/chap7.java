701efda2e7063119e742138c0bf1ec32c403f607
wokeup_at.4pm <25020258@vnu.edu.vn>
2026-05-02 23:06:52 +0700

    có lỗi không

--- TranDucManh_25020258_Week9/Bai07/src/main/java/chap7.java
+++ TranDucManh_25020258_Week9/Bai07/src/main/java/chap7.java
@@ -1,22 +1,15 @@
 package TranDucManh_25020258_Week9.Bai07.src;
 
-/**
- * Lớp chap7 thực hiện các phép tính toán cơ bản để minh họa CI/CD.
- */
-        public class chap7 {
+
+public class chap7 {
 
 
   public int add(int a, int b) {
     return a + b;
   }
 
-  /**
-   * Hàm kiểm tra số chẵn.
-   *
-   * @param number Số cần kiểm tra.
-   * @return true nếu là số chẵn, ngược lại là false.
-   */
-  public boolean isEven(int number) {
-    return number % 2 == 0;
+
+  public boolean isEven(int n                 ) {
+    return number%2 == 0;
   }
 }
